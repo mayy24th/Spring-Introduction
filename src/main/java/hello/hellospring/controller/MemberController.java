e56f607629e61ec2eb7afa478a223141ff732e5e
mayy24th <86235986+mayy24th@users.noreply.github.com>
2023-03-15 14:06:43 +0900

    Initial commit

---
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -0,0 +1,49 @@
+package hello.hellospring.controller;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+// 스프링 컨테이너라는 통이 생김 -> @Controller 어노테이션이 있으면 MemberController 객체를 생성해서 통에 넣어둠
+// => '스프링 컨테이너에서 스프링 빈이 관리된다' 라고 함
+public class MemberController {
+    private final MemberService memberService;
+
+    @Autowired
+    // memberService를 스프링이 스프링 컨테이너에 있는 memberService와 연결
+    // MemberController가 생성될 때 스프링 빈에 등록되어있는 memberService를 가져다 넣어줌 : DI (의존성 주입)
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    // 데이터 조회 Get
+    // 데이터 등록 Post
+    @GetMapping("/members/new")
+    public String CreateForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+}
